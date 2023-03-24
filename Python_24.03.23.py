7dfefa76f3a359ba94cfa48078b725124d94fc4d
Pasha1002 <kanavetspavel2001@gmail.com>
2023-03-24 22:41:14 +0200

    tusk dune

--- Python_24.03.23.py
+++ Python_24.03.23.py
@@ -1,1 +1,11 @@
 
+number_1 = int(input('Enter the number 1: '))
+number_2 = int(input('Enter the number 2: '))
+number_3 = int(input('Enter the number 3: '))
+value = int(input("\n 1-Sum \n 2-Product \n "))
+if value ==1:
+    print(number_1 + number_2 + number_3)
+elif value ==2:
+     print(number_1 * number_2 * number_3)
+else:
+    print("Choose from 1-2")
