c8f910ac0b45972d386d569f9b3b677d7c8c4003
Pasha1002 <kanavetspavel2001@gmail.com>
2023-03-24 21:52:04 +0200

    tusk dune

--- Python_24.03.23.py
+++ Python_24.03.23.py
@@ -1,1 +1,14 @@
 
+number_1 = int(input('Enter the number 1: '))
+number_2 = int(input('Enter the number 2: '))
+value = int(input("\n 1-Sum \n 2-Differense \n 3-Arithmetic mean \n 4-Product \n "))
+if value ==1:
+    print(number_1 + number_2)
+elif value ==2:
+    print(number_1 - number_2)
+elif value ==3:
+    print((number_1 + number_2)/2)
+elif value ==4:
+     print(number_1 * number_2)
+else:
+    print("Choose from 1-4")
