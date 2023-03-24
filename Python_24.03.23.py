6d6d607539d2671cd12af2bde2d1a9a1bcb73008
Pasha1002 <kanavetspavel2001@gmail.com>
2023-03-24 20:24:17 +0200

    tusk dune

--- Python_24.03.23.py
+++ Python_24.03.23.py
@@ -1,1 +1,8 @@
 
+number_1 = int(input('Enter the number 1: '))
+number_2 = int(input('Enter the number 2: '))
+
+if number_1 > number_2:
+    print(number_1)
+elif  number_2 > number_1:
+    print(number_2)
