0b889c4b53b85d4f0dc2e730f981d67aa127e794
Pasha1002 <kanavetspavel2001@gmail.com>
2023-03-25 16:07:00 +0200

    tusk dune

--- Python_24.03.23.py
+++ Python_24.03.23.py
@@ -1,1 +1,9 @@
 
+meters = int(input("Enter in meters: ")) 
+value = float(input("\n 1-Convert to miles \n 2-Сonvert to inches \n 3-Convert to yards \n "))
+if value ==1:
+    print(meters * 0.00062137)
+elif value ==2:
+    print(meters * 39.370)
+elif value ==3:
+    print(meters * 1.0936)
